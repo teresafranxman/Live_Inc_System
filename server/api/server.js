782381584a69b6ce9ac7c703006bcff26f467c10
DavJau2005 <du6358gk@go.minnstate.edu>
2026-04-20 17:55:25 -0600

    Update server port to 5000 and enhance startup message

--- server/api/server.js
+++ server/api/server.js
@@ -10,7 +10,7 @@ import { db } from "./config/db.js";
 
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 db.query("SELECT NOW()")
     .then(() => console.log("DB connected successfully"))
@@ -26,5 +26,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on LAN on port ${PORT}`);
 });
