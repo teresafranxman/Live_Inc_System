98581c576de231d08e29f58550b650dc2a1ee8a5
david_jauregui <du6358gk@go.minnstate.edu>
2026-04-19 22:25:00 -0500

    server.js update port definition before db check.

--- server/api/server.js
+++ server/api/server.js
@@ -6,11 +6,11 @@ dotenv.config();
 import app from "./index.js";
 import { db } from "./config/db.js";
 
+const PORT = process.env.PORT || 3000;
+
 db.query("SELECT NOW()")
     .then(() => console.log("DB connected successfully"))
     .catch(err => console.error("DB connection error:", err));
-
-const PORT = process.env.PORT || 3000;
 
 app.get("/db-check", async (req, res) => {
   const result = await db.query("SELECT current_database()");
